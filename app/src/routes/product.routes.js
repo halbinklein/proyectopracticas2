c2c73abc63f394271cd7cb46a404f807fb181e9f
sjuarez-growbiz <sjuarez@growbiz.la>
2024-03-09 02:30:40 -0500

    FC

--- app/src/routes/product.routes.js
+++ app/src/routes/product.routes.js
@@ -2,7 +2,7 @@
 
 const express = require('express')
 const router = express.Router()
-const ProductController = require('../controllers/client.controllers');
+const ProductController = require('../controllers/product.controllers');
 
 // Llamar a la función "saludar"
 router.get('/:nombre', ProductController.saludar);
